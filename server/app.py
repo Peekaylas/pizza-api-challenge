6dfd92dac96a6284440d59170662f429aa758894
Philip Katiku <peekaylas@gmail.com>
2025-06-18 01:04:21 +0300

    create app function added

--- server/app.py
+++ server/app.py
@@ -1,6 +1,21 @@
-from server import create_app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .controllers.restaurant_controller import restaurants_bp
+from .controllers.pizza_controller import pizzas_bp
+from .controllers.restaurant_pizza_controller import restaurant_pizzas_bp
 
-app = create_app()
+db = SQLAlchemy()
+migrate = Migrate()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('server.config.Config')
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    app.register_blueprint(restaurants_bp)
+    app.register_blueprint(pizzas_bp)
+    app.register_blueprint(restaurant_pizzas_bp)
+    
+    return app
